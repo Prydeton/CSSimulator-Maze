394ce781dd0466c429322d14359a8192595b2263
Prydeton <contact@maxwellreid.tech>
2021-01-08 19:26:45 +1100

    Add drag and hold cell modification

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { useState } from 'react'
 import MazeToolBar from './MazeToolBar'
 import MazeGrid from './MazeGrid'
 import generateMaze from './generateMaze'
-import {makeGridData} from './grid'
+import {makeGridData, cell} from './grid'
+import cloneDeep from 'lodash.clonedeep'
 
 import './style/App.css'
 
@@ -11,18 +12,38 @@ const toolOptions = ['empty', 'wall', 'start', 'end']
 
 const App = () => {
     const [width, height] = [17, 17]
-    const [gridData, setGridData] = useState(makeGridData(width, height))
+    const [gridData, setGridData] = useState(makeGridData(width, height, () => cell.WALL))
     const [tool, setTool] = useState(toolOptions[1])
 
     const handleGeneration = () => {
         setGridData(generateMaze(width, height))
     }
-    
+
+    const handleCellPress = (i, j) => {
+        let gd = cloneDeep(gridData)
+        switch (tool) {
+            case 'empty':
+                gd[i][j] = cell.EMPTY
+                break
+            case 'wall':
+                gd[i][j] = cell.WALL
+                break
+            case 'start':
+                gd[i][j] = cell.START
+                break
+            case 'end':
+                gd[i][j] = cell.END
+                break
+        }
+        console.log(tool)
+        console.log(i,j)
+        setGridData(gd)
+    }
 
     return (
         <div className='maze-app'>
             <MazeToolBar onToolChange={setTool} toolOptions={toolOptions} tool={tool} onGenerate={handleGeneration}/>
-            <MazeGrid gridData={gridData} />
+            <MazeGrid gridData={gridData} onCellPress={handleCellPress}/>
         </div>
     )
 }
--- src/MazeGrid.js
+++ src/MazeGrid.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {cell} from './grid'
-
 import './style/MazeGrid.css'
 
 const getClassesFromState = (state) => {
@@ -16,18 +15,27 @@ const getClassesFromState = (state) => {
     }
 }
 
-const MazeGrid = ({ gridData }) => <div className='maze-grid'>
-    { gridData.map((row,i) => <MazeGridRow key={i} rowData={row} />) }
+const MazeGrid = ({ gridData, onCellPress}) => <div className='maze-grid'>
+    { gridData.map((row,i) => <MazeGridRow i={i} key={i} rowData={row} onCellPress={onCellPress}/>) }
 </div>
 
-const MazeGridRow = ({ rowData }) => <div className='grid-row'>
-    { rowData.map((cell, i) => <MazeGridCell key={i} cellData={cell} />) }
+const MazeGridRow = ({ rowData, onCellPress, i}) => <div className='grid-row'>
+    { rowData.map((cell, j) => <MazeGridCell i={i} j={j} key={j} cellData={cell} onCellPress={onCellPress}/>) }
 </div>
 
-const MazeGridCell = ({ cellData }) => {
+const MazeGridCell = ({ cellData, onCellPress, i, j}) => {
     const classes = getClassesFromState(cellData)
+    
+    const handleCellSelection = (i, j, e) => {
+        console.log("test")
+        if (e.buttons > 0 && e.buttons < 4) {
+            onCellPress(i, j)
+            e.preventDefault()
+        }
+    }
+
     return (
-        <div className={`grid-cell ${classes.join(" ")}`}></div>
+        <div onClick={() => onCellPress(i, j)} onMouseOver={(e) => handleCellSelection(i,j,e)} onMouseDown={(e) => handleCellSelection(i,j,e)} className={`grid-cell ${classes.join(" ")}`}></div>
     )
 }
 
